52c2d649f2daec48ed5aa5342d438968628af23e
Syded74 <47427269+Syded74@users.noreply.github.com>
2024-09-08 18:55:35 +0000

    Update main.py to bind to the correct port

--- main.py
+++ main.py
@@ -82,5 +82,6 @@ def predict():
         app.logger.error(error_msg)
         return jsonify({'error': error_msg}), 500
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Bind to the port provided by Heroku
+    app.run(host="0.0.0.0", port=port)
